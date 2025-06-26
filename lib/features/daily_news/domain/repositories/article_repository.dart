554e8e090cdc47e65221b6afbfc75f55afe64361
Junsung Choi <choiexe1@gmail.com>
2025-06-26 15:31:40 +0900

    ArticleRepository 인터페이스 메서드 추가

--- lib/features/daily_news/domain/repositories/article_repository.dart
+++ lib/features/daily_news/domain/repositories/article_repository.dart
@@ -3,4 +3,10 @@ import 'package:clean_architecture_daily_news/features/daily_news/domain/entitie
 
 abstract interface class ArticleRepository {
   Future<Result<List<ArticleEntity>>> getNewsArticles();
+
+  Future<List<ArticleEntity>> getSavedArticles();
+
+  Future<void> saveArticle(ArticleEntity entity);
+
+  Future<void> deleteArticle(ArticleEntity entity);
 }
